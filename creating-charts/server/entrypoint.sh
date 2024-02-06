e7b25108329906f6247dd58968b3f9a50d78bd89
perplexedyawdie <sunkwenga@gmail.com>
2024-02-06 09:33:51 -0500

    updated entrypoint file

--- creating-charts/server/entrypoint.sh
+++ creating-charts/server/entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Wait for kubeconfig to be available
+while [ ! -f /output/kubeconfig.yaml ]; do
+    sleep 1
+done
+
 # Set the KUBECONFIG environment variable
 export KUBECONFIG=/output/kubeconfig.yaml
 echo "export KUBECONFIG=/output/kubeconfig.yaml" >> ~/.bashrc
 
-# Restrict permissions on kubeconfig file
-chmod 600 /output/kubeconfig.yaml
-
 # Update the cluster hostname in kubeconfig file
 sed -i 's/127.0.0.1/server/g' /output/kubeconfig.yaml
+
+# Restrict permissions on kubeconfig file
+chmod 600 /output/kubeconfig.yaml
 
 # Start the SSH server
 /usr/sbin/sshd -D
